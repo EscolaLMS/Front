07c6dbe83a924de96cd0b841d42e6c205c583e20
Mateusz <mateusz@qunabu.com>
2021-06-25 19:04:38 +0200

    first course preview

--- src/js/redux/courseProgram/reducer.test.js
+++ src/js/redux/courseProgram/reducer.test.js
@@ -423,7 +423,7 @@ describe("courses reducer", () => {
     expect(reducer(undefined, data)).toEqual({
       loading: false,
       error: false,
-      lessons: data.payload.lessons,
+      lessons: data.payload.data.lessons,
     });
   });
 });
