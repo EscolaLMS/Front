53900adcdc757223fe9188197f17eefdf7a0db4a
Mateusz Wojczal <mateusz@wojczal.com>
2021-10-12 10:38:23 +0200

    commit

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://escolalms-react.envytheme.com"
-    : "http://localhost:3000";
+    process.env.NODE_ENV === 'production'
+        ? 'https://escolalms-react.envytheme.com'
+        : 'http://localhost:3000';
 
 export default baseUrl;
--- src/utils/catchErrors.js
+++ src/utils/catchErrors.js
@@ -5,18 +5,18 @@ const catchErrors = (error, displayError) => {
         // console.error("Error response", errorMsg.description);
 
         // for image upload
-        if(error.response.data.error){
+        if (error.response.data.error) {
             errorMsg = error.response.data.error.message;
         }
     } else if (error.request) {
         // Request made but no response recieved
         errorMsg = error.request;
         // console.error("Error request", errorMsg);
-    } else  {
+    } else {
         // console.error("Error message", errorMsg);
     }
 
     displayError(errorMsg);
-}
+};
 
 export default catchErrors;
