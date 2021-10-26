6d42279f63c3325d1cf5c1b54d2c5cf7c47ea16d
Mateusz Cholcha <mateusz.cholcha@gmail.com>
2021-10-26 16:12:24 +0200

    remove unused packages

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import "./index.scss";
+import './index.scss';
 
 const SocialLinks: React.FC<{
-  className?: string
-}> = ({className = ''}) => {
+  className?: string;
+}> = ({ className = '' }) => {
   return (
     <ul className={`social-links ${className}`}>
       <li className="social-links__item">
         <a className="social-link social-link--facebook" href="#facebook" target="_blank">
-          <i className="bx bxl-facebook"/>
+          <i className="bx bxl-facebook" />
         </a>
       </li>
       <li className="social-links__item">
         <a className="social-link social-link--twitter" href="#twitter" target="_blank">
-          <i className="bx bxl-twitter"/>
+          <i className="bx bxl-twitter" />
         </a>
       </li>
       <li className="social-links__item">
         <a className="social-link social-link--instagram" href="#instagram" target="_blank">
-          <i className="bx bxl-instagram"/>
+          <i className="bx bxl-instagram" />
         </a>
       </li>
       <li className="social-links__item">
         <a className="social-link social-link--linkedin" href="#linkedin" target="_blank">
-          <i className="bx bxl-linkedin"/>
+          <i className="bx bxl-linkedin" />
         </a>
       </li>
     </ul>
