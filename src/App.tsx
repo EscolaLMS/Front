23185ffb717e94e3fe09dc06c57c84f382262ef0
Mateusz Cholcha <mateusz.cholcha@gmail.com>
2021-10-28 14:38:44 +0200

    remove old style.scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ import Routes from './components/Routes';
 import './style/css/bootstrap.min.css';
 import './style/css/boxicons.min.css';
 import './style/css/flaticon.css';
-import '../node_modules/react-modal-video/css/modal-video.min.css';
-import './style/css/style.scss'; // old
-import 'react-image-lightbox/style.css';
+import '../node_modules/react-modal-video/css/modal-video.min.css'; // TODO: move to component
+import 'react-image-lightbox/style.css'; // TODO: move to component?
 import './style/scss/index.scss';
 
 const App = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import { EscolaLMSContextProvider } from '@escolalms/sdk/lib/react/context';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WebFont from 'webfontloader';
 
 import './i18n';
+
+WebFont.load({
+  google: {
+    families: ['Nunito:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800'],
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,5 +29,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// TODO: ???
+// TODO: what with this?
 reportWebVitals();
