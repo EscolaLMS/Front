70250f00a7507c147fdbf7986e51a131ea4a2415
Mateusz Cholcha <mateusz.cholcha@gmail.com>
2021-11-22 22:49:29 +0100

    override babel

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,9 @@
 const { alias, configPaths, aliasJest } = require('react-app-rewire-alias');
 
+const { override, useBabelRc } = require('customize-cra');
+
 const aliasMap = configPaths('./tsconfig.paths.json'); // or jsconfig.paths.json
 
-module.exports = alias(aliasMap);
+module.exports = override(alias(aliasMap), useBabelRc());
+
 module.exports.jest = aliasJest(aliasMap);
