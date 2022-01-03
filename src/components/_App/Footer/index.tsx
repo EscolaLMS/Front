ef635c085600b5fdc440140ec58642c3cb7136df
Mateusz Cholcha <mateusz.cholcha@gmail.com>
2022-01-03 14:52:07 +0100

    use custom logo from settings

--- src/components/_App/Footer/index.tsx
+++ src/components/_App/Footer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../../images/logo.svg';
 import SocialLinks from '@/components/SocialLinks';
@@ -6,11 +6,16 @@ import { chunks } from '@/utils/array';
 import { API } from '@escolalms/sdk/lib';
 import { usePages } from '@/escolalms/sdk/hooks/usePages';
 import './index.scss';
+import { EscolaLMSContext } from '@escolalms/sdk/lib/react/context';
 
 // TODO: This is not a universal solution. Problem with translation of slugs (in url).
 const seperatedPagesSlugs = ['privacy-policy', 'terms-of-service'];
 
 const Footer = () => {
+  const { settings } = useContext(EscolaLMSContext);
+
+  const customLogo = settings.global?.logo || Logo;
+
   const { collection } = usePages();
 
   const currentYear = useMemo(() => new Date().getFullYear(), []);
@@ -85,7 +90,7 @@ const Footer = () => {
           <div className="col-lg-6 col-md-6 col-sm-6">
             <div className="single-footer-widget">
               <Link className="logo" to="/">
-                <img src={Logo} alt="logo" />
+                <img src={customLogo} alt="logo" />
               </Link>
 
               <p>
