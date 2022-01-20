224a4dc7974c31ebc525efef5a4bef4b32ac2723
tomekgubernat <tomekgubernat@gmail.com>
2022-01-20 10:49:15 +0100

    handling course visibility param

---
+++ src/components/Routes/configExtend.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+import { EscolaLMSContext } from '@escolalms/sdk/lib/react/context';
+import routes from './routes';
+
+const ConfigRoute: React.FC<RouteProps> = ({
+  component: Component,
+  ...rest
+}: // eslint-disable-next-line
+any) => {
+  const { authentication } = routes;
+  const { user, fetchConfig, config } = useContext(EscolaLMSContext);
+
+  React.useEffect(() => {
+    fetchConfig();
+  }, [fetchConfig]);
+
+  const platformVisibility = config.escolalms_courses.platform_visibility === 'public' || false;
+
+  const fullVisibility = config.escolalms_courses.course_visibility === 'show_all' || false;
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        platformVisibility || (user.value && user.value.id && fullVisibility) ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: authentication,
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default ConfigRoute;
