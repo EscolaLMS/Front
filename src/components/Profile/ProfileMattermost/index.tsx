45678296c7005421c89e2a48531ae61b352c810a
Przemek <przemek.wiktorski@escola.pl>
2022-02-01 14:43:37 +0100

    mattermost channels

--- src/components/Profile/ProfileMattermost/index.tsx
+++ src/components/Profile/ProfileMattermost/index.tsx
@@ -2,9 +2,8 @@ import React, { useContext, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { EscolaLMSContext } from '@escolalms/sdk/lib/react';
 import { API } from '@escolalms/sdk/lib';
-
-import './index.scss';
 import LmsBoxHeader from '@/components/Common/LmsBoxHeader';
+import './index.scss';
 
 const ProfileMattermost = () => {
   const { mattermostChannels, fetchMattermostChannels } = useContext(EscolaLMSContext);
@@ -12,91 +11,90 @@ const ProfileMattermost = () => {
 
   useEffect(() => {
     fetchMattermostChannels();
-  }, []);
+  }, [fetchMattermostChannels]);
 
   return (
     <React.Fragment>
-      {/* <div className="mattermostChannel">
+      <div className="mattermostChannel">
         <div className="container">
           <LmsBoxHeader text={t('Navbar.MyMattermostChannels')} />
 
-          {mattermostChannels && mattermostChannels?.value && mattermostChannels?.value.teams && mattermostChannels?.value.teams.length === 0 ? (
+          {mattermostChannels?.value?.teams?.length === 0 ? (
             <p className="text-center">{t('MyProfilePage.EmptyMattermostChannels')}</p>
           ) : (
             <form>
               <div className="categories-area ptb-100">
                 <div className="container">
+                  {(mattermostChannels?.value?.teams || []).map((team: API.MattermostChannels) => {
+                    return (
+                      <div className="row">
+                        <div className="col-lg-3 col-sm-6 col-md-6">
+                          <div className="single-categories-box">
+                            <img src="/images/categories/categorie1.jpg" alt="Team" />
 
-                   {(mattermostChannels?.value?.teams || []).map((Team: API.MattermostChannels) => {
-                      return (
-                        <div className="row">
-                          <div className="col-lg-3 col-sm-6 col-md-6">
-                            <div className="single-categories-box">
-                              <img src="/images/categories/categorie1.jpg" alt="Team" />
-
-                              <div className="content">
-                                <h3>{Team.display_name}</h3>
-                                <span>{`${Team.channels.length} ${t(
-                                  'MattermostChannel.Channels',
-                                )}`}</span>
-                              </div>
+                            <div className="content">
+                              <h3>{team.display_name}</h3>
+                              <span>{`${team.channels.length} ${t(
+                                'MattermostChannel.Channels',
+                              )}`}</span>
                             </div>
-                            <div className="widget widget_recent_courses">
-                              {Team.channels.map((channel: API.Channel) => (
-                                <>
-                                  <div className="boxes-area boxes-style-two bg-f5f7fa">
-                                    <div className="container">
-                                      <div className="col-12">
-                                        <div className="single-box-item">
-                                          <div className="item">
-                                            <a
-                                              href={channel.url}
-                                              className="thumb"
-                                              target="_blank"
-                                              rel="noreferrer"
-                                            >
-                                              <img
-                                                className="fullimage cover"
-                                                src={`${process.env.PUBLIC_URL}/images/mattermost_logo.png`}
-                                                alt="Mattermost"
-                                              />
-                                            </a>
+                          </div>
+                          <div className="widget widget_recent_courses">
+                            {team.channels.map((channel: API.Channel) => (
+                              <>
+                                <div className="boxes-area boxes-style-two bg-f5f7fa">
+                                  <div className="container">
+                                    <div className="col-12">
+                                      <div className="single-box-item">
+                                        <div className="item">
+                                          <a
+                                            href={channel.url}
+                                            className="thumb"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                          >
+                                            <img
+                                              className="fullimage cover"
+                                              src={`${process.env.PUBLIC_URL}/images/mattermost_logo.png`}
+                                              alt="Mattermost"
+                                            />
+                                          </a>
 
-                                            <div className="info">
-                                              <h4 className="title usmall">
-                                                {channel.type !== 'O' && (
-                                                  <i className="flaticon-password"></i>
-                                                )}{' '}
-                                                <a
-                                                  href={channel.url}
-                                                  target="_blank"
-                                                  rel="noreferrer"
-                                                >
-                                                  {channel.display_name}
-                                                </a>
-                                              </h4>
-                                              <p>{`${t('MattermostChannel.LastPost')}: ${new Date(
-                                                channel.last_post_at,
-                                              ).toLocaleDateString('en-US')}`}</p>
-                                            </div>
+                                          <div className="info">
+                                            <h4 className="title usmall">
+                                              {channel.type !== 'O' && (
+                                                <i className="flaticon-password"></i>
+                                              )}{' '}
+                                              <a
+                                                href={channel.url}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                              >
+                                                {channel.display_name}
+                                              </a>
+                                            </h4>
+                                            <p>{`${t('MattermostChannel.LastPost')}: ${new Date(
+                                              channel.last_post_at,
+                                            ).toLocaleDateString('en-US')}`}</p>
                                           </div>
                                         </div>
                                       </div>
                                     </div>
                                   </div>
-                                </>
-                              ))}
-                            </div>
+                                </div>
+                              </>
+                            ))}
                           </div>
                         </div>
-                      );
-                    })}
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
             </form>
           )}
         </div>
-      </div> */}
+      </div>
     </React.Fragment>
   );
 };
