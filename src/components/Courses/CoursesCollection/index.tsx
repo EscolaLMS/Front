db7d3362c802a481004a3af6cf91b38471b5fde9
Mateusz Motyka <mati@mateuszs-mini.home>
2022-06-02 18:21:42 +0200

    handle query for filters

--- src/components/Courses/CoursesCollection/index.tsx
+++ src/components/Courses/CoursesCollection/index.tsx
@@ -1,142 +1,286 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CoursesContext } from "@/components/Courses/CoursesContext";
 import { Title } from "@escolalms/components/lib/components/atoms/Typography/Title";
+import { Text } from "@escolalms/components/lib/components/atoms/Typography/Text";
 import { Button } from "@escolalms/components/lib/components/atoms/Button/Button";
+import { Dropdown } from "@escolalms/components/lib/components/molecules/Dropdown/Dropdown";
 import { useTranslation, Trans } from "react-i18next";
 import { API } from "@escolalms/sdk/lib";
+import { CourseCard } from "@escolalms/components/lib/components/molecules/CourseCard/CourseCard";
 import "./index.scss";
 import styled from "styled-components";
 import { EscolaLMSContext } from "@escolalms/sdk/lib/react";
 import { CloseIcon } from "../../../icons";
-
-enum Order {
-  ASC = "ASC",
-  DESC = "DESC",
-}
+import { useHistory } from "react-router-dom";
+import Pagination from "@/components/Pagination";
 
 const StyledHeader = styled.div`
   background: ${({ theme }) => theme.primaryColor};
   padding: 140px 40px 30px;
+  margin-bottom: 40px;
+
   h1 {
     color: ${({ theme }) => theme.white};
     margin-bottom: 40px;
   }
-  .categories-container {
+
+  .filters-container {
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-between;
+    width: 100%;
     align-items: center;
-    .clear-btn {
-      appearance: none;
+    .Dropdown-control {
       background: transparent;
-      border: none;
-      outline: none;
-      margin-left: 15px;
-      cursor: pointer;
+      border-color: transparent;
+      .Dropdown-placeholder {
+        color: ${({ theme }) => theme.white};
+      }
+      .Dropdown-arrow-wrapper {
+        svg {
+          filter: brightness(0) invert(1);
+        }
+      }
     }
-    .categories-row {
+    .categories-container {
       display: flex;
-      max-width: 500px;
-      overflow: scroll;
       justify-content: flex-start;
       align-items: center;
-      column-gap: 10px;
-      -ms-overflow-style: none;
-      scrollbar-width: none;
-      &::-webkit-scrollbar {
-        display: none;
+
+      .clear-btn {
+        appearance: none;
+        background: transparent;
+        border: none;
+        outline: none;
+        margin-left: 15px;
+        cursor: pointer;
       }
-      .single-category-btn {
-        border-color: ${({ theme }) => theme.white};
-        color: ${({ theme }) => theme.white};
-        max-height: 45px !important;
-        width: 100%;
-        &--active {
+
+      .categories-row {
+        display: flex;
+        max-width: 500px;
+        overflow: scroll;
+        justify-content: flex-start;
+        align-items: center;
+        column-gap: 10px;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        &::-webkit-scrollbar {
+          display: none;
+        }
+
+        .single-category-btn {
           border-color: ${({ theme }) => theme.white};
-          color: ${({ theme }) => theme.primaryColor};
-          background-color: ${({ theme }) => theme.white};
+          color: ${({ theme }) => theme.white};
+          max-height: 45px !important;
+          width: 100%;
+          &--active {
+            border-color: ${({ theme }) => theme.white};
+            color: ${({ theme }) => theme.primaryColor};
+            background-color: ${({ theme }) => theme.white};
+          }
         }
       }
     }
   }
 `;
 
-const CoursesCollection: React.FC<{ className?: string; itemCol?: number }> = ({
-  className = "",
-  itemCol = 6,
-}) => {
+const CoursesList = styled.section`
+  .course-wrapper {
+    margin-bottom: 75px;
+  }
+`;
+
+const CoursesCollection: React.FC = () => {
+  const { params, setParams, courses } = useContext(CoursesContext);
+  const { categoryTree } = useContext(EscolaLMSContext);
   const [selectedMainCategory, setSelectedMainCategory] =
     useState<API.Category | null>(null);
   const [selectedSubCategory, setSelectedSubCategory] =
     useState<API.Category | null>(null);
-  const { params, setParams, courses } = useContext(CoursesContext);
-  const { categoryTree } = useContext(EscolaLMSContext);
   const { t } = useTranslation();
+  const history = useHistory();
+  console.log(params);
+  const typeFilters = [
+    { label: "Darmowe", value: "true" },
+    { label: "Płatne", value: "false" },
+  ];
+
+  useEffect(() => {
+    if (params?.category_id) {
+      const categoryFromQuery =
+        categoryTree.list?.filter(
+          (item) => item.id === Number(params?.category_id)
+        )[0] || null;
+      setSelectedMainCategory(categoryFromQuery);
+    }
+  }, [categoryTree]);
+
+  useEffect(() => {
+    console.log(selectedMainCategory);
+  }, [selectedMainCategory]);
 
   if (courses && (!courses.list || !courses.list.data?.length)) {
     return <div className="col-lg-8">{t("NoCourses")}</div>;
   }
-  console.log(categoryTree);
-
   return (
     <>
       <StyledHeader>
         <Title level={1}>
           {selectedMainCategory ? selectedMainCategory.name : "Kursy"}
         </Title>
-        <div className="categories-container">
-          <div className="categories-row">
-            {!selectedMainCategory
-              ? categoryTree.list?.map((mainCategory) => (
-                  <Button
-                    onClick={() => setSelectedMainCategory(mainCategory)}
-                    mode="outline"
-                    className="single-category-btn"
-                  >
-                    {mainCategory.name}
-                  </Button>
-                ))
-              : selectedMainCategory &&
-                selectedMainCategory.subcategories &&
-                selectedMainCategory.subcategories.length === 0
-              ? categoryTree.list?.map((mainCategory) => (
-                  <Button
-                    onClick={() => setSelectedMainCategory(mainCategory)}
-                    mode="outline"
-                    className={`single-category-btn ${
-                      mainCategory.name === selectedMainCategory?.name &&
-                      "single-category-btn--active"
-                    }`}
-                  >
-                    {mainCategory.name}
-                  </Button>
-                ))
-              : selectedMainCategory?.subcategories &&
-                selectedMainCategory?.subcategories.map((subCategory) => (
-                  <Button
-                    onClick={() => setSelectedSubCategory(subCategory)}
-                    mode="outline"
-                    className={`single-category-btn ${
-                      subCategory.name === selectedSubCategory?.name &&
-                      "single-category-btn--active"
-                    }`}
-                  >
-                    {subCategory.name}
-                  </Button>
-                ))}
+        <div className="filters-container">
+          <div className="categories-container">
+            <div className="categories-row">
+              {!selectedMainCategory //Show main categories
+                ? categoryTree.list?.map((mainCategory) => (
+                    <Button
+                      onClick={() => {
+                        setSelectedMainCategory(mainCategory);
+                        setParams &&
+                          setParams({
+                            ...params,
+                            page: 1,
+                            category_id: mainCategory?.id,
+                          });
+                      }}
+                      mode="outline"
+                      className="single-category-btn"
+                    >
+                      {mainCategory.name}
+                    </Button>
+                  ))
+                : selectedMainCategory &&
+                  selectedMainCategory.subcategories &&
+                  selectedMainCategory.subcategories.length === 0 //Show main categories if main category is selected and subcategories does not exists
+                ? categoryTree.list?.map((mainCategory) => (
+                    <Button
+                      onClick={() => {
+                        setSelectedMainCategory(mainCategory);
+                        setParams &&
+                          setParams({
+                            ...params,
+                            page: 1,
+                            category_id: mainCategory?.id,
+                          });
+                      }}
+                      mode="outline"
+                      className={`single-category-btn ${
+                        mainCategory.name === selectedMainCategory?.name &&
+                        "single-category-btn--active"
+                      }`}
+                    >
+                      {mainCategory.name}
+                    </Button>
+                  ))
+                : selectedMainCategory?.subcategories && //Show subcategories
+                  selectedMainCategory?.subcategories.map((subCategory) => (
+                    <Button
+                      onClick={() => {
+                        setSelectedSubCategory(subCategory);
+                        setParams &&
+                          setParams({
+                            ...params,
+                            page: 1,
+                            category_id: subCategory?.id,
+                          });
+                      }}
+                      mode="outline"
+                      className={`single-category-btn ${
+                        subCategory.name === selectedSubCategory?.name &&
+                        "single-category-btn--active"
+                      }`}
+                    >
+                      {subCategory.name}
+                    </Button>
+                  ))}
+            </div>
+            {(selectedMainCategory || selectedSubCategory) && (
+              <button
+                className="clear-btn"
+                onClick={() => {
+                  setSelectedMainCategory(null);
+                  setSelectedSubCategory(null);
+                  setParams &&
+                    setParams({
+                      ...params,
+                      page: 1,
+                      category_id: undefined,
+                    });
+                }}
+              >
+                <CloseIcon />
+              </button>
+            )}
           </div>
-          {(selectedMainCategory || selectedSubCategory) && (
-            <button
-              className="clear-btn"
-              onClick={() => {
-                setSelectedMainCategory(null);
-                setSelectedSubCategory(null);
-              }}
-            >
-              <CloseIcon />
-            </button>
-          )}
+          <div className="selects-row">
+            <Dropdown
+              onChange={(e) =>
+                setParams &&
+                setParams({
+                  ...params,
+                  page: 1,
+                  free: e.value === "true" ? true : false,
+                })
+              }
+              placeholder="Typ szkolenia"
+              options={typeFilters}
+            />
+          </div>
         </div>
       </StyledHeader>
+      <CoursesList>
+        <div className="row">
+          {courses?.list?.data.map((item) => (
+            <div className="col-xl-3 col-lg-4 col-md-6">
+              <div className="course-wrapper">
+                <CourseCard
+                  id={item.id}
+                  title={item.title}
+                  categories={{
+                    categoryElements: item.categories || [],
+                    onCategoryClick: () => console.log("clicked"),
+                  }}
+                  onButtonClick={() => history.push(`/courses/${item.id}`)}
+                  buttonText="Zacznij teraz"
+                  lessonCount={5}
+                  hideImage={false}
+                  subtitle={
+                    item.subtitle ? (
+                      <Text>
+                        <strong style={{ fontSize: 14 }}>
+                          {item.subtitle?.substring(0, 30)}
+                        </strong>
+                      </Text>
+                    ) : null
+                  }
+                  image={{
+                    url: item.image_url,
+                    alt: "",
+                  }}
+                  tags={item.tags as API.Tag[]}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </CoursesList>
+      {courses &&
+        courses.list &&
+        courses.list.meta.total > courses.list.meta.per_page && (
+          <Pagination
+            total={courses.list.meta.total}
+            perPage={courses.list.meta.per_page}
+            currentPage={courses.list.meta.current_page}
+            onPage={(i) =>
+              setParams &&
+              setParams({
+                ...params,
+                page: i,
+                per_page: 6,
+              })
+            }
+          />
+        )}
     </>
   );
   // return (
--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -1,24 +1,18 @@
 import React, { useContext } from "react";
 
-import CoursesSidebar from "@/components/Courses/CoursesSidebar";
 import Layout from "@/components/_App/Layout";
 import { useTranslation } from "react-i18next";
 import CoursesCollection from "@/components/Courses/CoursesCollection";
 import CoursesProvider from "@/components/Courses/CoursesProvider";
 import { CoursesContext } from "@/components/Courses/CoursesContext";
-import PageBanner from "@/components/Common/PageBanner";
-import { API } from "@escolalms/sdk/lib";
 
 const CoursesPage = () => {
-  const { t } = useTranslation();
-  const { params, setParams, courses } = useContext(CoursesContext);
-  console.log(courses);
   return (
     <Layout>
       <CoursesProvider onlyFree={false}>
         <section className="courses-page">
           <div className="container">
-            <CoursesCollection className="col-lg-8 col-md-12" />
+            <CoursesCollection />
           </div>
         </section>
       </CoursesProvider>
