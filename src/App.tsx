6cf46e816c2b6bc5ed437d871c79f237b9f96219
Mateusz Wojczal <mateusz@wojczal.com>
2022-06-21 12:18:17 +0200

    remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { useCallback, useContext, useEffect, lazy } from "react";
 
 import Routes from "./components/Routes";
+
 import "./style/css/bootstrap.min.css";
-import "./style/css/boxicons.min.css";
-import "./style/css/flaticon.css";
-import "../node_modules/react-modal-video/css/modal-video.min.css";
-import "react-image-lightbox/style.css";
+
 import styled, { createGlobalStyle } from "styled-components";
 import { isMobile } from "react-device-detect";
 import { EscolaLMSContext } from "@escolalms/sdk/lib/react";
