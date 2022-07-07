2eeb9f89a6479b10306ac452d435a2a297ce76a4
pieecia <piotr.kananowicz@escolasoft.com>
2022-07-07 09:02:04 +0200

    fix translation

--- src/components/SingleCoursesTwo/CoursesDetailsSidebar/index.tsx
+++ src/components/SingleCoursesTwo/CoursesDetailsSidebar/index.tsx
@@ -140,7 +140,7 @@ const CoursesDetailsSidebar: React.FC<{ course: API.Course }> = ({
         {course.users_count ? (
           <IconText
             icon={<IconSquares />}
-            text={`${t("CoStursePage.Students")}: ${course.users_count}`}
+            text={`${t("CoursePage.Students")}: ${course.users_count}`}
           />
         ) : (
           ""
