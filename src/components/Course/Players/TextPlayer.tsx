70aa17af26a7c2dc26e84dfa892e4af2e0e5f3f3
Piotr <piotr.kananowicz@escolasoft.com>
2022-08-01 13:09:41 +0200

    LMSDEMO-527 - hyperlink style (#314)

--- src/components/Course/Players/TextPlayer.tsx
+++ src/components/Course/Players/TextPlayer.tsx
@@ -1,5 +1,6 @@
 import { MarkdownRenderer } from "@escolalms/components/lib/components/molecules/MarkdownRenderer/MarkdownRenderer";
 import React, { ReactElement, FunctionComponent, useEffect } from "react";
+import { Link } from "@escolalms/components/lib/components/atoms/Link/Link";
 
 const fontSizes = ["small", "regular", "bigger", "big"];
 
@@ -16,7 +17,15 @@ const TextPlayer: FunctionComponent<{
     <div
       className={`center-image-richtext typebase size-${fontSizes[fontSize]}`}
     >
-      {value && <MarkdownRenderer>{value}</MarkdownRenderer>}
+      {value && (
+        <MarkdownRenderer
+          components={{
+            a: (props) => <Link {...props} underline />,
+          }}
+        >
+          {value}
+        </MarkdownRenderer>
+      )}
     </div>
   );
 };
