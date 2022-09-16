a98574ce584262108cc75f52d94927090ad54b04
Mateusz Wojczal <mateusz@wojczal.com>
2022-09-16 14:22:36 +0200

    Scorm player

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -59,6 +59,7 @@ const CourseProgram = () => {
 
   useEffect(() => {
     if (id) {
+      console.log("fetch program");
       fetchProgram(Number(id));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
