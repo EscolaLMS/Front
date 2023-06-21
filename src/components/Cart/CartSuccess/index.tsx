79b6732577685fc3036bb5c7153870c4bf07e59b
Przemek <przemodesign@gmail.com>
2023-06-21 17:02:56 +0200

    [WELLMS-347] Add CartSuccess component. (#399)
    * Update sdk package to latest.
    Add CartSuccess component.
    
    * Fix static-analysis issue.

---
+++ src/components/Cart/CartSuccess/index.tsx
@@ -0,0 +1,74 @@
+import { useContext, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { EscolaLMSContext } from "@escolalms/sdk/lib/react/context";
+import Layout from "@/components/_App/Layout";
+import Container from "@/components/Container";
+import { Button } from "@escolalms/components/lib/components/atoms/Button/Button";
+
+const CartSuccessPageStyled = styled.section`
+  .cart-success-container {
+    font-family: ${({ theme }) => theme.font};
+    background: #f8f8f8;
+    border: 1px solid ${({ theme }) => theme.primaryColor};
+    padding: 16px;
+    display: grid;
+    place-content: center;
+    gap: 16px;
+
+    .cart-success-title {
+      font-size: 24px;
+      font-weight: 500;
+      text-align: center;
+      margin: 0.8em;
+    }
+
+    .cart-success-text {
+      font-size: 18px;
+      text-align: center;
+    }
+
+    .cart-success-buttons {
+      display: grid;
+      grid-auto-flow: column;
+      gap: 8px;
+      justify-content: center;
+      margin: 0.8em;
+    }
+  }
+`;
+
+const CartSuccess = () => {
+  const { t } = useTranslation();
+  const { push } = useHistory();
+  const { fetchProgress } = useContext(EscolaLMSContext);
+
+  useEffect(() => {
+    fetchProgress();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Layout metaTitle={t("Cart.Cart")}>
+      <CartSuccessPageStyled>
+        <Container>
+          <div className="cart-success-container">
+            <h2 className="cart-success-title">{t("Cart.ThankYouTitle")}</h2>
+            <p className="cart-success-text">{t("Cart.ThankYouText")}</p>
+            <div className="cart-success-buttons">
+              <Button mode="primary" onClick={() => push("/user/my-profile")}>
+                {t("Menu.Profile")}
+              </Button>
+              <Button mode="primary" onClick={() => push("/courses")}>
+                {t("Menu.Courses")}
+              </Button>
+            </div>
+          </div>
+        </Container>
+      </CartSuccessPageStyled>
+    </Layout>
+  );
+};
+
+export default CartSuccess;
