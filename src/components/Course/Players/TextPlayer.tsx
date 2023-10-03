18f36963b0cdc60a5d955e2a3d2d8f6c3b98987c
Przemek <przemodesign@gmail.com>
2023-10-03 15:41:24 +0200

    Add resources download section inside TextPlayer component. (#419)

--- src/components/Course/Players/TextPlayer.tsx
+++ src/components/Course/Players/TextPlayer.tsx
@@ -1,6 +1,10 @@
-import React, { ReactElement, FunctionComponent, useEffect } from "react";
+import { ReactElement, FunctionComponent, useEffect } from "react";
 import styled from "styled-components";
+import { useTranslation } from "react-i18next";
 import { MarkdownRenderer } from "@escolalms/components/lib/components/molecules/MarkdownRenderer/MarkdownRenderer";
+import { API } from "@escolalms/sdk/lib";
+import { Download } from "@escolalms/components/lib/components/atoms/Download/Download";
+import { Text } from "@escolalms/components/lib/components/atoms/Typography/Text";
 
 const TextPlayerStyles = styled.div`
   h1 {
@@ -23,7 +27,10 @@ const TextPlayerStyles = styled.div`
 const TextPlayer: FunctionComponent<{
   value?: string;
   onLoad?: () => void;
-}> = ({ value, onLoad }): ReactElement => {
+  resources?: API.TopicResource[];
+}> = ({ value, onLoad, resources }): ReactElement => {
+  const { t } = useTranslation();
+
   useEffect(() => {
     value && onLoad && onLoad();
   }, [value, onLoad]);
@@ -31,6 +38,14 @@ const TextPlayer: FunctionComponent<{
   return (
     <TextPlayerStyles>
       {value && <MarkdownRenderer>{value}</MarkdownRenderer>}
+      {resources && (
+        <>
+          <Text>{t("CoursePage.Resources")}</Text>
+          {resources.map(({ name, url }) => (
+            <Download href={url} fileName={name} />
+          ))}
+        </>
+      )}
     </TextPlayerStyles>
   );
 };
