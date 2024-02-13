7ac8b8dfc265e90df5f12092c2c3fdeb42c77128
Przemek <przemek.wiktorski@escola.pl>
2024-02-13 09:32:13 +0100

    local package

--- switch_to_local_components.sh
+++ switch_to_local_components.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 yarn remove "@escolalms/components"
-yarn add jssha @escolalms/ts-models umi-request
 git clone git@github.com:EscolaLMS/components.git
 cp tsconfig.paths.json.local tsconfig.paths.json
 echo "switched to local"
