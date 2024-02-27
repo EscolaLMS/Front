5a68b0c9b5c40f2ee371cb586be0ad9ac20576f8
Przemek <przemek.wiktorski@escola.pl>
2024-02-27 17:36:03 +0100

    kub build

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,5 @@ export const getAPIURL = () => {
 };
 
 // Usage
-export const API_URL = import.meta.env.VITE_APP_PUBLIC_API_URL;
+export const API_URL =
+  window.VITE_APP_API_URL || import.meta.env.VITE_APP_PUBLIC_API_URL || null;
