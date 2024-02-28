55d1ab95dbdcc119ea5b6ef9ae6528d92c33fcfb
Tomasz Smolarek <59400506+dyfero@users.noreply.github.com>
2024-02-28 09:04:12 +0100

    Build dev image (#437)
    * Build dev image
    
    ---------
    
    Co-authored-by: Tomasz Smolarek <tomasz.smolarek@escolasoft.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,4 @@ export const getAPIURL = () => {
 };
 
 // Usage
-export const API_URL = import.meta.env.VITE_APP_PUBLIC_API_URL;
+export const API_URL = window.VITE_APP_API_URL || (import.meta.env.VITE_APP_PUBLIC_API_URL || null);
