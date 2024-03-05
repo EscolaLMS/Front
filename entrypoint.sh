8e022225f5a55caa19015f562111c5b170bf4955
Tomasz Smolarek <59400506+dyfero@users.noreply.github.com>
2024-03-05 13:08:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ set -e
 rm -f /usr/local/apache2/logs/httpd.pid
 
 cp /usr/local/apache2/htdocs/tpl.html /usr/local/apache2/htdocs/index.html
-sed -ie "s~API_URL=null~API_URL=\"$API_URL\"~" /usr/local/apache2/htdocs/index.html
-sed -ie "s~SENTRYDSN=null~SENTRYDSN=\"$SENTRYDSN\"~" /usr/local/apache2/htdocs/index.html
-sed -ie "s~YBUG_ID=null~YBUG_ID=\"$YBUG_ID\"~" /usr/local/apache2/htdocs/index.html
-sed -ie "s~BASENAME=null~BASENAME=\"$BASENAME\"~" /usr/local/apache2/htdocs/index.html
-sed -ie "s~ROUTING_TYPE=null~ROUTING_TYPE=\"$ROUTING_TYPE\"~" /usr/local/apache2/htdocs/index.html
+sed -ie "s~API_URL = null~API_URL=\"$API_URL\"~" /usr/local/apache2/htdocs/index.html
+sed -ie "s~SENTRYDSN = null~SENTRYDSN=\"$SENTRYDSN\"~" /usr/local/apache2/htdocs/index.html
+sed -ie "s~YBUG_ID = null~YBUG_ID=\"$YBUG_ID\"~" /usr/local/apache2/htdocs/index.html
+sed -ie "s~BASENAME = null~BASENAME=\"$BASENAME\"~" /usr/local/apache2/htdocs/index.html
+sed -ie "s~ROUTING_TYPE = null~ROUTING_TYPE=\"$ROUTING_TYPE\"~" /usr/local/apache2/htdocs/index.html
 
 echo "API URL= " $API_URL
 echo "SENTRYDSN= " $SENTRYDSN
