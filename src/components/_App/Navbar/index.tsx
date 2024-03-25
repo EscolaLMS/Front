2efc44a3c3288dea98824af8003f87a5674d7ee3
Przemek <przemek.wiktorski@escola.pl>
2024-03-25 14:47:12 +0100

    logo extended

--- src/components/_App/Navbar/index.tsx
+++ src/components/_App/Navbar/index.tsx
@@ -337,13 +337,14 @@ const Navbar = () => {
       <StyledHeader>
         <Navigation
           mobile
-          logo={{
-            src: settings?.value?.global?.logo || Logo,
-            width: 150,
-            height: 50,
-            onClick: () => history.push(routeRoutes.home),
-            alt: "Logo",
-          }}
+          logo={
+            <Link to="/" aria-label={t("Go to the main page")}>
+              <ResponsiveImage
+                path={settings?.value?.global?.logo || ""}
+                srcSizes={[100, 200, 300]}
+              />
+            </Link>
+          }
           cart={
             user?.id ? (
               <div className="icons-container">
