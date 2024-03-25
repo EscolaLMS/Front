3ffbb9eaace64936fbfc6b1c424099456d32544c
myslaf <przemodesign@gmail.com>
2024-03-25 12:34:25 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@50948993c00fe6f966036efc8439e4282ea19272 🚀

---
+++ assets/AuthWrapper-DRk2eYM-.js
@@ -0,0 +1,13 @@
+import{as as e,q as r,j as s}from"./index-BZyw651V.js";var i=e();const a=r.div`
+  min-height: calc(100vh - 452px);
+  height: 700px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({theme:t})=>t.gray4};
+  @media (max-width: 991px) {
+    padding-top: 100px;
+    height: 100%;
+    padding-bottom: 50px;
+  }
+`,p=({children:t})=>s.jsx(a,{children:t});export{p as A,i as R};
