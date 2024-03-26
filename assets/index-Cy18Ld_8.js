cb11f5a4dfc90676c9c7fdda018436ac81e87af1
victazzz <przemyslaw.wiktorskii@gmail.com>
2024-03-26 12:31:23 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@e6e38c89ff106d38a0b1e988ef06c10bde5d8c7e 🚀

---
+++ assets/index-Cy18Ld_8.js
@@ -0,0 +1,87 @@
+import{q as x,G as p,r as n,f as u,j as s,aB as m,x as a,aj as b,y as g,h as j,o as y,i as f,T,V as $}from"./index-mArHxJMN.js";import{u as k}from"./usePayment-HEDCuteG.js";const S=x.div`
+  border-radius: ${({theme:e})=>e.buttonRadius}px;
+  background-color: ${({theme:e})=>p(e.mode,e.black,e.white,"black")};
+  padding: 22px;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 6px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+  .${"cancelled"} {
+    color: ${({theme:e})=>e.primaryColor};
+    @media (max-width: 768px) {
+      padding-left: 20px;
+    }
+  }
+`,N=({activeSubscription:e,subscriptionCancel:t})=>{const[r,i]=n.useState((e==null?void 0:e.status)||"active"),{t:o}=u(),d=n.useCallback(()=>{i("cancelled"),t(e.id)},[e==null?void 0:e.id,t]);return s.jsxs(S,{className:"info-box",children:[s.jsxs("div",{children:[e&&s.jsx(m,{}),s.jsx(a.Text,{children:e?o("Subscriptions.ActiveSubscription",{date:b(e.end_date,"dd.MM.yyyy")}):o("Subscriptions.NoSubscription")})]}),r==="cancelled"&&s.jsx(a.Text,{className:"cancelled",children:o("Subscriptions.Cancelled")}),r==="active"&&s.jsx(g.Button,{onClick:d,children:"Anuluj subskrypcję"})]})},w=x.div`
+  border-radius: ${({theme:e})=>e.cardRadius}px;
+  border: 1px solid ${({theme:e})=>e.gray3};
+  padding: ${({$isMobile:e})=>e?"20px":"23px 75px"};
+  transition: border 0.3s ease-in-out;
+  margin-bottom: ${({$isMobile:e})=>"30px"};
+  background-color: ${({theme:e})=>p(e.mode,e.black,e.white,"black")};
+  .content {
+    text-align: center;
+    max-width: 210px;
+    position: relative;
+    margin: 0 auto;
+
+    p {
+      margin-bottom: 0;
+    }
+    .information {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 2.5px;
+      margin-top: 5px;
+      background-color: ${({theme:e})=>e.secondaryColor};
+    }
+    .divider {
+      width: 21px;
+      height: 3px;
+      border-radius: 18px;
+      background-color: ${({theme:e})=>e.primaryColor};
+      margin: 23px auto 16px auto;
+    }
+    .description {
+      margin-bottom: 17px;
+    }
+    .price {
+      margin-bottom: 30px;
+    }
+    button {
+      width: 100%;
+    }
+    .tag {
+      position: absolute;
+      top: -37px;
+      left: 0;
+      right: 0;
+      margin: auto;
+      border-radius: 18px;
+      width: fit-content;
+      background-color: ${({theme:e})=>e.textColor};
+      padding: 3px 14px;
+
+      p {
+        text-transform: uppercase;
+        color: ${({theme:e})=>e.white};
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  &:hover {
+    border: 1px solid #333333;
+  }
+`,_=({subscription:e})=>{var l;const{t}=u(),{buySubscriptionByP24:r,user:i}=k(),o=j(),d=n.useMemo(()=>e.tags&&e.tags.includes("best-deal"),[e.tags]),h=n.useCallback(()=>{var c;(c=i.value)!=null&&c.id?r(e.id):o.push(y.login)},[e.id,(l=i.value)==null?void 0:l.id,r,o]);return s.jsx(w,{$isMobile:f,children:s.jsxs("div",{className:"content",children:[d&&s.jsx("div",{className:"tag",children:s.jsx(a.Text,{size:"13",children:t("Subscriptions.CheapestOffer")})}),s.jsx(a.Text,{children:t("Subscriptions.AccessVia")}),s.jsxs(T.Title,{level:1,as:"h4",children:[e.subscription_duration," ",t(`Subscriptions.Periods.${e.subscription_period}`)]}),s.jsxs(a.Text,{className:"information",size:"13",children:[s.jsx(m,{})," ",e.trial_duration,"-",t(`Subscriptions.Periods.${e.trial_period}`)," ",t("Subscriptions.TrialText")]}),s.jsx("div",{className:"divider"}),s.jsx(a.Text,{size:"13",className:"description",children:e.name}),s.jsxs(a.Text,{size:"24",className:"price",bold:!0,children:[$(e.gross_price)," zł"]}),s.jsx(g.Button,{mode:"secondary",onClick:()=>h(),children:t("Subscriptions.IPick")})]})})};export{N as A,_ as S};
---
+++ assets/my-certificates-DjNioMsE.js
@@ -0,0 +1,29 @@
+import{q as f,r as x,H as C,f as m,j as t,x as s,z as p,p as j,T as h,Z as y,bm as b}from"./index-mArHxJMN.js";import{P}from"./index-bu1CBlyu.js";import{u as T}from"./useDownloadCertificate-BjIKTpKN.js";import"./index-CuLncN6z.js";const w=f.section`
+  .empty-certificates-message {
+    background: ${({theme:e})=>e.mode==="dark"?e.gray1:e.gray5};
+  }
+  .buttons-container {
+    margin-top: 20px;
+    display: flex;
+
+    align-items: center;
+    justify-content: flex-start;
+
+    .download-btn {
+      all: unset;
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      cursor: pointer;
+      color: ${({theme:e})=>e.primaryColor};
+      p {
+        color: ${({theme:e})=>e.primaryColor};
+      }
+    }
+  }
+  .certificate-card {
+    margin-bottom: 24px;
+  }
+`,E=()=>{var o,l,n,c,d;const{certificates:e,fetchCertificates:r}=x.useContext(C.EscolaLMSContext),{t:a}=m(),{downloadCertificate:u,loadingId:g}=T();return x.useEffect(()=>{r()},[r]),t.jsx(t.Fragment,{children:t.jsxs(w,{children:[((o=e.list)==null?void 0:o.data.length)===0&&t.jsx(s.Text,{className:"empty-certificates-message",children:t.jsx("strong",{children:a("MyProfilePage.EmptyCertificates")})}),t.jsx(p.Row,{children:e&&((l=e==null?void 0:e.list)==null?void 0:l.data)&&((n=e.list)==null?void 0:n.data.length)>0&&((d=(c=e==null?void 0:e.list)==null?void 0:c.data)==null?void 0:d.filter(i=>i.title).map(i=>t.jsx(p.Col,{lg:4,children:t.jsx(j.CertificateCard,{uptitle:t.jsx(s.Text,{size:"13",children:a("CoursePage.CourseTitle")}),title:t.jsx(h.Title,{level:4,as:"h3",children:i.title}),dateUptitle:t.jsx(s.Text,{size:"13",children:a("CoursePage.CertificateDate")}),date:t.jsx(s.Text,{noMargin:!0,size:"16",bold:!0,children:new Date(i.created_at).toLocaleDateString("pl-PL")}),actions:t.jsx("div",{className:"buttons-container",children:g===i.id?t.jsx(y,{width:"15px",height:"15px"}):t.jsxs("button",{className:"download-btn",onClick:()=>u(i.id,i.title),children:[t.jsx(b,{})," ",t.jsx(s.Text,{bold:!0,size:"13",children:a("CoursePage.DownloadCertificate")})]})})})})))})]})})},L=f.div`
+  margin-top: 20px;
+`,v=()=>{const{t:e}=m();return t.jsx(P,{title:e("MyProfilePage.MyCertificates"),children:t.jsx(L,{children:t.jsx(E,{})})})};export{v as default};
