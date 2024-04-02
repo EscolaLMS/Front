88364007b74eb3ec73071eafa9cb4cb93e2b4267
victazzz <przemyslaw.wiktorskii@gmail.com>
2024-04-02 08:09:15 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@e9d1b4500a42916794cb67e0b472a4c6c6789170 🚀

---
+++ assets/index-BfwuSfu3.js
@@ -0,0 +1,10 @@
+import{q as a,j as o,bf as s}from"./index-CR0NW4tM.js";import{M as d}from"./index-DA5p0CY7.js";const t=a.div`
+  margin-bottom: 36px;
+  p {
+    color: ${({theme:r})=>r.mode==="dark"?r.gray5:r.gray2}!important;
+  }
+  a {
+    text-decoration: none;
+    color: ${({theme:r})=>r.mode==="dark"?r.gray5:r.gray2}!important;
+  }
+`,m=({items:r})=>o.jsx(t,{children:o.jsx(d,{children:o.jsx(s.BreadCrumbs,{items:r})})});export{m as B};
