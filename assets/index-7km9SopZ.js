5f29198c327285034bc2e652ed41002430e045f9
mtmotyka <32429974+mtmotyka@users.noreply.github.com>
2024-04-26 11:47:30 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@6ac9427832be5d529ccff40ac268000f653e3f57 🚀

---
+++ assets/index-7km9SopZ.js
@@ -0,0 +1,162 @@
+import{n as K,h as E,r as i,d as _,aa as te,aH as se,ab as D,j as t,q as M,N as F,f as R,E as G,T as L,x as B,al as ne,y as A,an as re,aI as ae,J as oe,aJ as ie,a4 as V,aK as le,aL as ce,aM as de,p as ue,i as U,L as q,s as pe,m as J,W as me,o as fe,V as $,v as ge,C as he}from"./index-1htdI_EN.js";import{a as xe,b as be,C as z}from"./CourseCard-BwIPHb6k.js";import{R as ye}from"./index-C7_XNX0t.js";import{b as je,Q as Ce}from"./questionnaires-BRJKE0B3.js";import{u as ve}from"./useDownloadCertificate-DeGiL-7a.js";import{q as Pe}from"./base-C7euzKDi.js";import{P as we}from"./index-CPDbNd4v.js";import{S as _e}from"./swiper-DCdm1N59.js";import{a as H}from"./a11y-IQo-zzUa.js";const X=()=>{const{search:e,pathname:d}=K(),r=E(),c=i.useMemo(()=>new URLSearchParams(e),[e]),a=i.useCallback(p=>new URLSearchParams(e).get(p),[e]),x=i.useCallback(p=>new URLSearchParams(e).getAll(p),[e]),b=i.useCallback((p,f)=>{!f||f===""?c.delete(p):c.set(p,f),r.push({pathname:d,search:c.toString()})},[r,d,c]),y=i.useCallback(p=>{const f=new URLSearchParams;return Object.entries(p).forEach(([v,g])=>{Array.isArray(g)?g.forEach(P=>{f.append(`${v}`,P)}):f.append(v,g)}),f.toString()},[]),u=i.useCallback(p=>{r.push({pathname:d,search:p?y(p):void 0})},[r,y,d]),j=i.useCallback((p,f)=>{c.delete(p),f.forEach(v=>{c.append(p,`${v}`)}),r.push({pathname:d,search:c.toString()})},[r,d,c]),C=i.useMemo(()=>Pe.parse(e),[e]);return{query:c,getQueryValueByName:a,getAllQueryValueByName:x,setQueryParam:b,setPathname:u,setQueryArrayParam:j,paramsToObject:C,objectToParam:y}};var Y={};(function(e){var d=_&&_.__makeTemplateObject||function(n,s){return Object.defineProperty?Object.defineProperty(n,"raw",{value:s}):n.raw=s,n},r=_&&_.__assign||function(){return r=Object.assign||function(n){for(var s,l=1,h=arguments.length;l<h;l++){s=arguments[l];for(var o in s)Object.prototype.hasOwnProperty.call(s,o)&&(n[o]=s[o])}return n},r.apply(this,arguments)},c=_&&_.__createBinding||(Object.create?function(n,s,l,h){h===void 0&&(h=l);var o=Object.getOwnPropertyDescriptor(s,l);(!o||("get"in o?!s.__esModule:o.writable||o.configurable))&&(o={enumerable:!0,get:function(){return s[l]}}),Object.defineProperty(n,h,o)}:function(n,s,l,h){h===void 0&&(h=l),n[h]=s[l]}),a=_&&_.__setModuleDefault||(Object.create?function(n,s){Object.defineProperty(n,"default",{enumerable:!0,value:s})}:function(n,s){n.default=s}),x=_&&_.__importStar||function(n){if(n&&n.__esModule)return n;var s={};if(n!=null)for(var l in n)l!=="default"&&Object.prototype.hasOwnProperty.call(n,l)&&c(s,n,l);return a(s,n),s};Object.defineProperty(e,"__esModule",{value:!0}),e.Tabs=void 0;var b=t,y=x(i),u=x(te),j=se,C=D,p=D,f=(0,u.default)("div")(g||(g=d([`
+  .tabs-menu {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+    background-color: `,`;
+    border-radius: `,`px;
+    padding: 8px;
+    &-inner {
+      display: flex;
+    }
+  }
+
+  .tab-menu-btn {
+    position: relative;
+    padding: 6px 24px;
+    appearance: none;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    font-size: 16px;
+    white-space: nowrap;
+    font-family: `,`;
+    color: `,`;
+    border-radius: `,`px;
+    &:first-of-type {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+    &:nth-of-type(2) {
+      border-radius: 0;
+    }
+    &:last-of-type {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+
+    background-color: #f8f8f8;
+    &.active {
+      font-weight: bold;
+      background-color: `,`;
+      color: `,`;
+      border-radius: `,`px;
+    }
+  }
+
+  .tabs-panel {
+    padding: 25px 0;
+  }
+`],[`
+  .tabs-menu {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow: auto;
+    -webkit-overflow-scrolling: touch;
+    background-color: `,`;
+    border-radius: `,`px;
+    padding: 8px;
+    &-inner {
+      display: flex;
+    }
+  }
+
+  .tab-menu-btn {
+    position: relative;
+    padding: 6px 24px;
+    appearance: none;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    font-size: 16px;
+    white-space: nowrap;
+    font-family: `,`;
+    color: `,`;
+    border-radius: `,`px;
+    &:first-of-type {
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+    }
+    &:nth-of-type(2) {
+      border-radius: 0;
+    }
+    &:last-of-type {
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+
+    background-color: #f8f8f8;
+    &.active {
+      font-weight: bold;
+      background-color: `,`;
+      color: `,`;
+      border-radius: `,`px;
+    }
+  }
+
+  .tabs-panel {
+    padding: 25px 0;
+  }
+`])),function(n){var s=n.theme;return(0,C.getStylesBasedOnTheme)(s.mode,s.black,s.white)},function(n){var s=n.theme;return s.cardRadius},function(n){var s=n.theme;return(0,j.getFontFromTheme)(s).fontFamily},function(n){var s=n.theme;return(0,C.getStylesBasedOnTheme)(s.mode,s.white,s.gray1)},function(n){var s=n.theme;return s.buttonRadius},function(n){var s=n.theme;return s.primaryColor},function(n){var s=n.theme;return s.white},function(n){var s=n.theme;return s.buttonRadius}),v=function(n){var s=n.tabs,l=s===void 0?[]:s,h=n.defaultActiveKey,o=h===void 0?l[0].key:h,m=n.onClick,w=n.className,k=w===void 0?"":w,N=y.useState(o),I=N[0],ee=N[1],Q=l&&l.find(function(T){return T.key===I});return(0,b.jsxs)(f,r({className:"wellms-component ".concat(k)},{children:[(0,b.jsx)("div",r({className:"tabs-menu"},{children:(0,b.jsx)("div",r({className:"tabs-menu-inner"},{children:l.map(function(T){return T.hidden?null:(0,b.jsx)("button",r({type:"button",className:"tab-menu-btn ".concat(I===T.key?"active":""),id:(0,p.getUniqueId)("tab-menu-".concat(T.key)),onClick:function(){ee(T.key),m&&m(T.key)}},{children:T.label}),T.key)})}))})),(0,b.jsx)("div",r({id:"tabpanel-".concat(I),className:"tabs-panel"},{children:(0,b.jsx)(y.Fragment,{children:Q&&Q.component})}))]}))};e.Tabs=v,e.default=(0,u.withTheme)((0,u.default)(e.Tabs)(P||(P=d([""],[""]))));var g,P})(Y);const Te=M.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`,Se=({children:e})=>t.jsx(Te,{children:e}),Me=({courseData:e,visible:d,onClose:r})=>{const{sendProgress:c}=i.useContext(F.EscolaLMSContext),{t:a}=R(),{push:x}=E(),b=i.useCallback(async()=>{await c(e.course.id,e.progress.map(({topic_id:y})=>({topic_id:y,status:0}))),x(`/course/${e.course.id}`)},[e.course.id,e.progress,c,x]);return t.jsx(G.Modal,{animation:"zoom",maskAnimation:"fade",destroyOnClose:!0,visible:d,onClose:r,children:t.jsxs(t.Fragment,{children:[t.jsx(L.Title,{level:4,children:a("ResetProgressModal.Continue")}),t.jsxs(B.Text,{children:[" ",a("ResetProgressModal.RestartCourse")]}),t.jsxs(ne,{$gap:16,children:[t.jsx(A.Button,{mode:"primary",onClick:b,children:a("ResetProgressModal.WantContinue")}),t.jsx(A.Button,{mode:"primary",onClick:r,children:a("ResetProgressModal.Cancel")})]})]})})},ke=M(re)`
+  gap: 6px;
+  margin: 0;
+  width: 100%;
+  p {
+    margin: 0;
+  }
+`,Re=({courseId:e})=>{const{fetchCertificates:d}=i.useContext(F.EscolaLMSContext),{downloadCertificate:r,loadingId:c}=ve(),{t:a}=R(),[x,b]=i.useState(!1),y=i.useCallback(async()=>{try{const u=await d({assignable_type:ae.CertificateAssignableTypes.Course,assignable_id:Number(e)});if(u&&u.success){const j=u.data||[];if(j.length===0){b(!0);return}j.forEach(({id:C,title:p})=>{r(C,p)})}}catch(u){oe(`${a("UnexpectedError")}`,"error"),console.log(u)}},[e,r,a,d]);return t.jsx(Z,{onClick:y,children:!x&&t.jsxs(t.Fragment,{children:[t.jsx(ie,{})," ",a("MyProfilePage.DownloadCertificate")," ",c>-1&&t.jsx(V,{width:"10px",height:"10px"})]})})},Z=M.button`
+  all: unset;
+  text-decoration: underline;
+  font-size: 13px;
+  font-family: ${({theme:e})=>e.font};
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`,Ne=M.div`
+  margin-top: 5px;
+`,Oe=({courseData:e,courseProgress:d})=>{const[r,c]=i.useState(void 0),[a,x]=i.useState(!1),{fetchQuestionnaires:b,fetchQuestionnaire:y}=i.useContext(F.EscolaLMSContext),[u,j]=i.useState({show:!1,step:0,loading:!1}),{t:C}=R(),p={isDone:e.finish_date,isActive:e.start_date&&!e.finish_date,isNotStarted:!e.start_date&&!e.finish_date},f=i.useMemo(()=>e.deadline?new Date(e.deadline):null,[e.deadline]),v=f?f.getTime()<Date.now():!1,g=i.useMemo(()=>f?le(f):null,[f]),[P,n]=i.useState([]),s=i.useCallback(()=>{if(c(void 0),j(l=>({...l,show:!1})),u.step<P.length-1){j(h=>({...h,step:h.step+1}));const l=setTimeout(()=>{j(h=>({...h,show:!0}))},500);return()=>clearTimeout(l)}},[P,u.step]);return i.useEffect(()=>{r&&je({courseId:r,fetchQuestionnaire:y,fetchQuestionnaires:b,onSucces:l=>{n(l)},onFinish:()=>j(l=>({...l,loading:!1}))})},[r]),t.jsxs(ke,{children:[p.isDone&&t.jsx(Re,{courseId:e.course.id}),d===100&&t.jsxs(t.Fragment,{children:[t.jsxs(Z,{onClick:()=>{c(e.course.id),j(l=>({...l,show:!0,loading:!0}))},children:[t.jsx(ce,{})," ",C("MyProfilePage.RateCourse")," ",u.loading&&t.jsx(V,{width:"10px",height:"10px"})]}),t.jsx(Ne,{children:!v&&p.isDone&&t.jsx(A.Button,{mode:"secondary",onClick:()=>x(!0),children:C("MyProfilePage.ResetCourseProgress")})})]}),!!v&&g!==null&&g[0]<0&&t.jsxs(B.Text,{size:"12",children:[C("MyProfilePage.AccessCourseExpired")," ",de.format(g[0],g[1])]}),t.jsx(Me,{courseData:e,visible:a,onClose:()=>x(!1)}),u.show&&r&&!u.loading&&(P.length?t.jsx(t.Fragment,{children:t.jsx(ye,{course:Ce.COURSE,courseId:r,visible:u.show,onClose:s,questionnaire:P[u.step],onFinish:()=>{}})}):t.jsx(G.Modal,{onClose:s,visible:u.show,animation:"zoom",maskAnimation:"fade",destroyOnClose:!0,width:468,children:t.jsx(L.Title,{style:{textAlign:"center"},children:C("CourseProgram.CourseRated")})}))]})},W=({course:e})=>{const d=E();return t.jsx(Se,{children:t.jsx(ue.NewCourseCard,{mobile:U,id:e.id,image:t.jsx(q,{to:`/course/${e.id}`,children:e.image_path?t.jsx(pe.ResponsiveImage,{path:e.image_path,alt:e.title,srcSizes:[300,600,900]}):t.jsx(xe,{})}),title:t.jsx(q,{to:`/courses/${e.id}`,className:"title",children:t.jsx(L.Title,{level:4,as:"h2",children:e.title})}),categories:t.jsx(be,{categories:e.categories,onCategoryClick:r=>{d.push(`/courses/?categories[]=${r}`)}}),actions:e.courseData&&t.jsx(Oe,{courseData:e.courseData,courseProgress:e.progress||0}),progress:e.progress&&e.progress!==100&&!isNaN(e.progress)?{currentProgress:e.progress,maxProgress:100}:void 0})})},Ae=M.div`
+  overflow: hidden;
+  .course-wrapper {
+    margin-bottom: 24px;
+
+    a {
+      text-decoration: none;
+    }
+  }
+`,Ee=M.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  /* background: ${({theme:e})=>e.mode==="dark"?e.gray1:e.gray5}; */
+  padding: ${U?"80px 20px":"192px 20px"};
+  width: calc(100% - 30px);
+  margin: 0 auto;
+  text-align: center;
+  .small-text {
+    font-size: 14px;
+    margin-top: 20px;
+  }
+`,Le=M.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+`,O=({filter:e=S.ALL})=>{var l,h;const{fetchMyAuthoredCourses:d,myAuthoredCourses:r,fetchPaginatedProgress:c,paginatedProgress:a}=i.useContext(J.EscolaLMSContext),[x,b]=i.useState([]),y=E(),{t:u}=R(),{setQueryParam:j}=X(),{search:C}=K(),p=new URLSearchParams(C),f=p.get("page"),v=p.get("status"),g=i.useMemo(()=>{var o,m;return e===S.AUTHORED?(o=r.list)==null?void 0:o.meta:(m=a.list)==null?void 0:m.meta},[e,(l=r.list)==null?void 0:l.meta,(h=a.list)==null?void 0:h.meta]),P=i.useCallback(o=>{let m="";switch(o){case 1:case 5:break;case 2:m="started";break;case 4:m="finished";break}return m},[]);i.useEffect(()=>{c({page:f?parseInt(f):1,per_page:8,status:P(Number(v))}),(e===S.ALL||e===S.AUTHORED)&&d()},[c,e,d,f,v,P]);const n=i.useMemo(()=>{var o;return(((o=a.list)==null?void 0:o.data)||[]).reduce((m,w)=>({...m,[w.course.id?w.course.id:-1]:Math.round((w.progress||[]).reduce((k,N)=>N.status===1?k+1:k,0)/w.progress.length*100)}),{})},[a]),s=i.useCallback(o=>o.map(m=>({...m.course,courseData:m,progress:n[m.course.id?m.course.id:-1]})),[n]);return i.useEffect(()=>{var o,m,w,k;e!==S.AUTHORED?b(s(((o=a.list)==null?void 0:o.data)||[])):e===S.AUTHORED?b(((m=r.list)==null?void 0:m.data)||[]):b([...s(((w=a.list)==null?void 0:w.data)||[]),...((k=r.list)==null?void 0:k.data)||[]])},[e,a,r,s]),t.jsxs(Ae,{children:[x.length===0&&!a.loading&&!r.loading&&t.jsxs(Ee,{children:[t.jsx(L.Title,{level:3,children:u("MyProfilePage.EmptyCoursesTitle")}),t.jsx(B.Text,{className:"small-text",children:u("MyProfilePage.EmptyCoursesText")}),t.jsx(me,{children:t.jsx(A.Button,{onClick:()=>y.push(fe.courses),mode:"secondary",children:u("MyProfilePage.EmptyCoursesBtnText")})})]}),U?t.jsxs(_e,{children:[a.loading&&r.loading&&Array.from({length:8}).map((o,m)=>t.jsx(H,{className:"single-slide",children:t.jsx(z,{})},`skeleton-card-${m}`)),(!a.loading||!r.loading)&&x&&x.map(o=>t.jsx(H,{className:"single-slide",children:t.jsx(W,{course:o})},o.id))]}):t.jsxs(t.Fragment,{children:[t.jsx($.Row,{children:a.loading||r.loading?Array.from({length:8}).map((o,m)=>t.jsx($.Col,{md:3,children:t.jsx(z,{})},`skeleton-card-${m}`)):x.map(o=>t.jsx($.Col,{md:3,children:t.jsx(W,{course:o})},o.id))}),t.jsx(Le,{children:t.jsx(we,{total:(g==null?void 0:g.total)||0,perPage:Number((g==null?void 0:g.per_page)||0),currentPage:Number((g==null?void 0:g.current_page)||1),onPage:o=>{j("page",`${o}`),window==null||window.scrollTo(0,0)}})})]})]})},Ie=()=>{const{user:e}=i.useContext(J.EscolaLMSContext),d=i.useMemo(()=>{var c,a;return!!((a=(c=e.value)==null?void 0:c.roles)!=null&&a.includes("tutor"))},[e]),r=i.useMemo(()=>{var c,a;return!!((a=(c=e.value)==null?void 0:c.roles)!=null&&a.includes("student"))},[e]);return{isTutor:d,isStudent:r}};var S=(e=>(e.IN_PROGRESS="inProgress",e.PLANNED="planned",e.FINISHED="finished",e.AUTHORED="authored",e.ALL="all",e))(S||{});const $e=M.section`
+  background-color: ${({theme:e})=>e.gray4};
+  .courses-wrapper {
+    padding-top: 100px;
+
+    min-height: fit-content;
+    @media (max-width: 991px) {
+      margin-top: 0;
+      min-height: fit-content;
+    }
+  }
+`,Fe=()=>{const{t:e}=R(),{isTutor:d}=Ie(),{query:r,setQueryParam:c}=X(),a={tabs:[{label:e("MyProfilePage.ALlCourses"),key:1,component:t.jsx(O,{filter:"all"})},{label:e("MyProfilePage.InProgress"),key:2,component:t.jsx(O,{filter:"inProgress"})},{label:e("MyProfilePage.Finished"),key:4,component:t.jsx(O,{filter:"finished"})},{label:e("MyProfilePage.Authored"),key:5,component:t.jsx(O,{filter:"authored"}),hidden:!d}],defaultActiveKey:Number(r.get("status")||1)};return t.jsx(ge,{children:t.jsx($e,{children:t.jsx(he,{children:t.jsx("div",{className:"courses-wrapper",children:t.jsx(Y.Tabs,{onClick:x=>{c("status",String(x)),c("page","1")},tabs:a.tabs,defaultActiveKey:a.defaultActiveKey})})})})})},Ge=Object.freeze(Object.defineProperty({__proto__:null,CourseStatus:S,default:Fe},Symbol.toStringTag,{value:"Module"}));export{S as C,Ge as i,X as u};
---
+++ assets/index-B5A7LIb6.js
@@ -0,0 +1,90 @@
+import{q as w,f as D,h as N,j as e,C as E,T as M,i as k,k as L,I as R,l as A,r as l,m as U,n as V,o as O,p as W,L as _,s as q,v as Q,B as F,M as G}from"./index-1htdI_EN.js";import{S as J,a as P}from"./a11y-IQo-zzUa.js";import{D as B}from"./index-C563vvu-.js";import{S as $}from"./swiper-DCdm1N59.js";import{C as K,a as X,b as Y}from"./CourseCard-BwIPHb6k.js";import{u as Z,C as x}from"./index-7km9SopZ.js";import"./index-oPrPogON.js";import"./useFetchCourses-CXSo1nEJ.js";import"./index-tpLXMDV6.js";import"./index-C7_XNX0t.js";import"./questionnaires-BRJKE0B3.js";import"./useDownloadCertificate-DeGiL-7a.js";import"./base-C7euzKDi.js";import"./index-CPDbNd4v.js";const ee=w.section`
+  overflow: hidden;
+
+  @media (max-width: 768px) {
+    padding: 30px 0;
+  }
+  h2 {
+    margin-bottom: 27px;
+  }
+  .slider-title {
+    @media (max-width: 575px) {
+      padding-right: 45%;
+    }
+  }
+  .categories-slider {
+    .slick-dots {
+      bottom: 40px;
+    }
+  }
+  .slick-track {
+    display: flex;
+    gap: 0 20px;
+  }
+  .slick-slide {
+    @media (max-width: 991px) {
+      display: flex !important;
+      justify-content: center;
+    }
+  }
+  .single-category-slide {
+    margin: 0 8px;
+
+    @media (max-width: 991px) {
+      margin: 0 auto;
+      min-width: 300px;
+    }
+  }
+  .category-card-icon {
+    img {
+      margin: 0 auto;
+      display: block;
+      max-height: 70px;
+    }
+  }
+`,se=w.div`
+  display: grid;
+  grid-auto-columns: minmax(0, 1fr);
+  grid-auto-flow: column;
+  grid-gap: 10px;
+`,ae=({categories:i})=>{const{t}=D(),o=N(),c=i.filter(s=>s.count&&s.count>0);return e.jsx(ee,{children:e.jsxs(E,{children:[e.jsx(M.Title,{level:1,as:"h2",children:e.jsx("strong",{children:t("Homepage.CategoriesTitle")})}),k?e.jsx("div",{className:"categories-slider",children:e.jsx(J,{spaceBetween:18,slidesOffsetAfter:18,breakpoints:{0:{slidesPerView:1.3},576:{slidesPerView:2},768:{slidesPerView:3},1201:{slidesPerView:4}},children:c.slice(-5).map(s=>e.jsx(P,{children:e.jsx(L.CategoryCard,{icon:e.jsx("img",{src:s.icon,alt:s.name}),title:s.name,buttonText:t("Homepage.CategoryBtnText"),subtitle:e.jsx(R.IconText,{icon:e.jsx(A,{}),text:`${t("CoursesLength",{count:s.count})}`}),onButtonClick:()=>o.push(`/courses/?categories[]=${s.id}`),variant:"gradient"})},s.id))})}):e.jsx(se,{children:c.slice(-5).map(s=>e.jsx("div",{className:"category-item",children:e.jsx(L.CategoryCard,{icon:e.jsx("img",{src:s.icon,alt:s.name}),title:s.name,buttonText:t("Homepage.CategoryBtnText"),subtitle:e.jsx(R.IconText,{icon:e.jsx(A,{}),text:`${t("CoursesLength",{count:s.count})}`}),onButtonClick:()=>o.push(`/courses/?categories[]=${s.id}`),variant:"gradient"})},s.id))})]})})},te=(i=x.ALL)=>{var H,I;const{fetchMyAuthoredCourses:t,myAuthoredCourses:o,fetchPaginatedProgress:c,paginatedProgress:s}=l.useContext(U.EscolaLMSContext),[a,d]=l.useState([]),y=N(),{setQueryParam:b}=Z(),{search:f}=V(),g=new URLSearchParams(f),m=g.get("page"),u=g.get("status"),v=l.useMemo(()=>{var n,r;return i===x.AUTHORED?(n=o.list)==null?void 0:n.meta:(r=s.list)==null?void 0:r.meta},[i,(H=o.list)==null?void 0:H.meta,(I=s.list)==null?void 0:I.meta]),h=l.useCallback(n=>{let r="";switch(n){case 1:case 5:break;case 2:r="started";break;case 3:r="planned";break;case 4:r="finished";break}return r},[]);l.useEffect(()=>{c({page:m?parseInt(m):1,per_page:18,status:h(Number(u))}),(i===x.ALL||i===x.AUTHORED)&&t()},[c,i,t,m,u,h]);const j=l.useMemo(()=>{var n;return(((n=s.list)==null?void 0:n.data)||[]).reduce((r,p)=>({...r,[p.course.id?p.course.id:-1]:Math.round((p.progress||[]).reduce((T,z)=>z.status===1?T+1:T,0)/p.progress.length*100)}),{})},[s]),C=l.useCallback(n=>n.map(r=>({...r.course,courseData:r,progress:j[r.course.id?r.course.id:-1]})),[j]);return l.useEffect(()=>{var n,r,p,T;i!==x.AUTHORED?d(C(((n=s.list)==null?void 0:n.data)||[])):i===x.AUTHORED?d(((r=o.list)==null?void 0:r.data)||[]):d([...C(((p=s.list)==null?void 0:p.data)||[]),...((T=o.list)==null?void 0:T.data)||[]])},[i,s,o,C]),{coursesToMap:a,paginationMeta:v,loading:s.loading||o.loading,handlePagination:n=>{b("page",`${n}`),window==null||window.scrollTo(0,0)},redirectToCourses:()=>y.push(O.courses)}},re=w.div`
+  position: relative;
+`,oe=({titleText:i,slidesPerView:t=4})=>{const{coursesToMap:o,loading:c}=te(),s=N();return o.length===0?null:e.jsxs(re,{children:[e.jsx("div",{className:"header-wrapper",children:e.jsx(M.Title,{className:"slider-title",level:1,as:"h2",children:e.jsx("strong",{children:i})})}),c&&e.jsx($,{slidesPerView:t,children:Array.from({length:6}).map((a,d)=>e.jsx(P,{children:e.jsx(K,{})},d))}),!c&&o&&e.jsx($,{slidesPerView:t,children:o.map(a=>e.jsx(P,{children:e.jsx(W.NewCourseCard,{mobile:k,id:a.id,image:e.jsx(_,{to:`/courses/${a.id}`,children:a.image_path?e.jsx(q.ResponsiveImage,{path:a.image_path,alt:a.title,srcSizes:[300,600,900]}):e.jsx(X,{})}),title:e.jsx(_,{to:`/courses/${a.id}`,children:e.jsx(M.Title,{level:3,as:"h3",className:"title",children:a.title})}),categories:e.jsx(Y,{categories:a.categories,onCategoryClick:d=>{s.push(`/courses/?categories[]=${d}`)}}),progress:a.progress&&a.progress!==100&&!isNaN(a.progress)?{currentProgress:a.progress,maxProgress:100}:void 0})},a.id))})]})},ie=w.div`
+  @media (max-width: 1200px) {
+    margin-top: 0;
+  }
+  @media (max-width: 575px) {
+    margin-top: -30px;
+  }
+  .home-hero {
+    margin-bottom: 45px;
+    padding-top: 42px;
+
+    h1 {
+      margin-top: 0 !important;
+    }
+    @media (max-width: 768px) {
+      margin-bottom: 40px;
+      padding-top: 100px;
+      h1 {
+        font-size: 26px;
+      }
+    }
+  }
+
+  .home-newest-courses {
+    margin-bottom: 30px;
+  }
+`,S=w(E)`
+  .header-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 14px;
+    button {
+      @media (max-width: 1200px) {
+        display: none;
+      }
+    }
+  }
+`,ye=()=>{var y,b,f,g,m,u,v,h,j;const{categoryTree:i,settings:t,fetchCategories:o,user:c}=l.useContext(U.EscolaLMSContext),s=N(),{t:a,i18n:d}=D();return l.useEffect(()=>{o()},[o]),e.jsx(Q,{metaTitle:a("Home"),children:e.jsxs(ie,{children:[e.jsx("section",{className:"home-hero",children:((y=t.value)==null?void 0:y.homepage)&&((b=t.value.homepage)==null?void 0:b.heroBannerText)&&((f=t.value.homepage)==null?void 0:f.heroBannerImg)&&((g=t.value.homepage)==null?void 0:g.heroBannerImg)!==""&&e.jsx(E,{children:e.jsx(F.Banner,{mobile:k,title:e.jsx(G.MarkdownRenderer,{children:`<h1>${((m=t.value.homepage)==null?void 0:m.heroBannerText[d.language])||""}</h1>`}),btnText:a("Homepage.HeroBtnText"),asset:e.jsx(q.ResponsiveImage,{path:((v=(u=t==null?void 0:t.value)==null?void 0:u.homepage)==null?void 0:v.heroBannerImg)||"",srcSizes:[500,750,1e3]}),handleBtn:()=>s.push(O.courses)})})}),((h=c.value)==null?void 0:h.id)&&e.jsx("section",{className:"home-newest-courses",children:e.jsx(S,{children:e.jsx(oe,{titleText:a("Navbar.MyCourses")})})}),e.jsx("section",{className:"home-newest-courses",children:e.jsx(S,{children:e.jsx(B,{titleText:a("Homepage.CoursesSlider2Title"),params:{per_page:8,order_by:"created_at",order:"DESC"}})})}),e.jsx("section",{className:"home-best-courses",children:e.jsx(S,{children:e.jsx(B,{titleText:a("Homepage.CoursesSlider1Title"),params:{per_page:8}})})}),e.jsx("div",{className:"promoted-courses-wrapper",children:e.jsx(S,{children:e.jsx(B,{titleText:a("Homepage.AwardedCoursesTitle"),params:{per_page:8},isSlider:!!k,ctaButton:!0})})}),i&&e.jsx("div",{className:"categories-section-wrapper",children:e.jsx(ae,{categories:((j=i.list)==null?void 0:j.filter(C=>!!C.icon))||[]})})]})})};export{ye as default};
