dc71eb0dc010d9674d09d3bff1dc96d3e5cb11a9
victazzz <przemyslaw.wiktorskii@gmail.com>
2024-05-06 07:51:12 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@f163cb55761c7f19829ff19aea0ba336268fe43e 🚀

--- assets/index-oPrPogON.js
+++
@@ -1,8 +0,0 @@
-import{q as a,i as j,j as n,x as d,a1 as i}from"./index-1htdI_EN.js";const o=a.div`
-  display: flex;
-  flex-direction: column;
-
-  .pricing-card-discount {
-    margin: 0;
-  }
-`,m=({price:l,taxRate:c,oldPrice:s,textSizes:r})=>j?n.jsxs(o,{children:[s&&n.jsx("div",{className:"pricing-card-discount",children:n.jsxs(d.Text,{size:(r==null?void 0:r.old)||"18",children:[i(s,c)," zł"]})}),n.jsxs(d.Text,{size:(r==null?void 0:r.new)||"16",children:[i(l,c)," zł"]})]}):n.jsxs(o,{children:[n.jsxs(d.Text,{size:(r==null?void 0:r.new)||"16",children:[i(l,c)," zł"]}),s&&n.jsx("div",{className:"pricing-card-discount",children:n.jsxs(d.Text,{size:(r==null?void 0:r.old)||"18",children:[i(s,c)," zł"]})})]});export{m as P};
