554c3eeab7bf563302ba31f4129bd763af0ffb57
victazzz <przemyslaw.wiktorskii@gmail.com>
2024-07-10 12:19:46 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@1c7a82232d0335a92db639fa1d46e6264389ba48 🚀

---
+++ assets/index-FmlbN0d3.js
@@ -0,0 +1,13 @@
+import{q as n,h as o,i as a,j as t,x as i,C as r,T as c,z as l,A as x,p as u}from"./index-BrDgRaEo.js";const d=n.div`
+  height: calc(100vh - 452px);
+  padding-top: 122px;
+  background-color: ${({theme:e})=>e.gray4};
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    row-gap: 30px;
+  }
+`,p=()=>{const{t:e}=o(),s=a();return t.jsx(i,{children:t.jsx(d,{children:t.jsx(r,{children:t.jsxs("div",{className:"content",children:[t.jsx(c.Title,{level:3,children:e("Custom404Page.Info")}),t.jsx(l.Text,{children:e("Custom404Page.NotFound")}),t.jsx(x.Button,{onClick:()=>s.push(u.home),children:e("Home")})]})})})})};export{p as default};
