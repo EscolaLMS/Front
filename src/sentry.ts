d0fd3fcb607dbc7a3c8c8cbe98c92aa5463fa08f
Mateusz Wojczal <mateusz@qunabu.com>
2024-11-18 15:57:58 +0100

    updating sentry package (#509)
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,5 +1,4 @@
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 
 declare global {
   interface Window {
@@ -14,8 +13,17 @@ function configSentry() {
   if (SENTRYDSN && window.location.hostname.indexOf("localhost") === -1) {
     Sentry.init({
       dsn: SENTRYDSN,
-      integrations: [new BrowserTracing()],
-      tracesSampleRate: 1,
+      integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+      ],
+      // Tracing
+      tracesSampleRate: 0.1, //  Capture 100% of the transactions
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      // tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     });
   }
   return null;
