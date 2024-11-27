c7642ff333d967fed2bb5184efcb13d18f53327a
Mateusz Wojczal <mateusz@wojczal.com>
2024-11-27 14:03:56 +0100

    Images REK-51

--- config/php/index.php
+++ config/php/index.php
@@ -5,6 +5,7 @@ $content = file_get_contents("index.html");
 // check variables first 
 
 $domains = [];
+$token = '<script><!-- inject env variables --></script>';
 
 if (isset($_ENV['MULTI_DOMAINS'])) {
     foreach (explode(",", $_ENV['MULTI_DOMAINS']) as $domain) {
@@ -24,35 +25,11 @@ if (is_file(dirname(__FILE__) . "/env_config.php")) {
 }
 
 if (key_exists($_SERVER['HTTP_HOST'], $domains) || key_exists($_SERVER['SERVER_NAME'], $domains)) {
-
     $setup = key_exists($_SERVER['HTTP_HOST'], $domains) ? $domains[$_SERVER['HTTP_HOST']] : $domains[$_SERVER['SERVER_NAME']];
-
     if (isset($setup)) {
-        if (isset($setup['VITE_APP_API_URL'])) {
-            $content = preg_replace('/(?<=window.VITE_APP_API_URL=")(.*)(?=")/', $setup['VITE_APP_API_URL'], $content);
-        }
-        if (isset($setup['VITE_APP_SENTRYDSN'])) {
-            $content = preg_replace('/(?<=window.VITE_APP_SENTRYDSN=")(.*)(?=")/', $setup['VITE_APP_SENTRYDSN'], $content);
-        }
-        if (isset($setup['VITE_APP_YBUG_ID'])) {
-            $content = preg_replace('/(?<=window.VITE_APP_YBUG_ID=")(.*)(?=")/', $setup['VITE_APP_YBUG_ID'], $content);
-        }
-        if (isset($setup['VITE_APP_BASENAME'])) {
-            $content = preg_replace('/(?<=window.VITE_APP_BASENAME=")(.*)(?=")/', $setup['VITE_APP_BASENAME'], $content);
-        }
-        if (isset($setup['VITE_APP_ROUTING_TYPE'])) {
-            $content = preg_replace('/(?<=window.VITE_APP_ROUTING_TYPE=")(.*)(?=")/', $setup['VITE_APP_ROUTING_TYPE'], $content);
-        }
-        if (isset($setup['VITE_APP_URL'])) {
-            $content = preg_replace('/(?<=window.VITE_APP_URL=")(.*)(?=")/', $setup['VITE_APP_URL'], $content);
-        }
-        if (isset($setup['VITE_APP_PUBLIC_IMG_URL'])) {
-            $content = preg_replace('/(?<=window.VITE_APP_PUBLIC_IMG_URL=")(.*)(?=")/', $setup['VITE_APP_PUBLIC_IMG_URL'], $content);
-        }
-
         foreach ($setup as $key => $value) {
-            if (!in_array($key, ['VITE_APP_API_URL', 'VITE_APP_SENTRYDSN', 'VITE_APP_YBUG_ID', 'VITE_APP_BASENAME', 'VITE_APP_ROUTING_TYPE', 'VITE_APP_URL', 'VITE_APP_PUBLIC_IMG_URL'])) {
-                $content = str_replace('</head>', "\n" . '<script>window.' . $key . '="' . $value . '"</script></head>', $content);
+            if (str_starts_with($key, 'VITE_APP_') || str_starts_with($key, 'REACT_APP_')) {
+                $content = str_replace($token, "\n" . '<script>window.' . $key . '="' . $value . '"</script>' . $token, $content);
             }
         }
     }
