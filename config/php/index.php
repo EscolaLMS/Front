9f68edc1d7f8a2dfdfb7a79e8a7104c56403abcc
Mateusz Wojczal <mateusz@qunabu.com>
2024-11-27 15:53:29 +0100

    Feature/mod rewrite (#516)
    * rewrite all
    
    * Images REK-51
    
    * Images REK-51
    
    ---------
    
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>

--- config/php/index.php
+++ config/php/index.php
@@ -5,7 +5,7 @@ $content = file_get_contents("index.html");
 // check variables first 
 
 $domains = [];
-$token = '<script><!-- inject env variables --></script>';
+$token = '<!-- inject env variables -->';
 
 if (isset($_ENV['MULTI_DOMAINS'])) {
     foreach (explode(",", $_ENV['MULTI_DOMAINS']) as $domain) {
@@ -29,7 +29,7 @@ if (key_exists($_SERVER['HTTP_HOST'], $domains) || key_exists($_SERVER['SERVER_N
     if (isset($setup)) {
         foreach ($setup as $key => $value) {
             if (str_starts_with($key, 'VITE_APP_') || str_starts_with($key, 'REACT_APP_')) {
-                $content = str_replace($token, "\n" . '<script>window.' . $key . '="' . $value . '"</script>' . $token, $content);
+                $content = str_replace($token, "\n" . 'window.' . $key . '="' . $value . '"' . $token, $content);
             }
         }
     }
