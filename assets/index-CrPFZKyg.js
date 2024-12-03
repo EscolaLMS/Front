3106a00025f5b84b82ea37ca85384b1ffdb18d39
victazzz <przemyslaw.wiktorskii@gmail.com>
2024-12-03 08:16:48 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@96f716ba30c348ff83cc6fd845ebf61da8a96278 🚀

--- assets/index-CrPFZKyg.js
+++
@@ -1,8 +0,0 @@
-import{q as a,o as j,j as n,y as d,W as l}from"./index-Ccx-J0-t.js";const i=a.div`
-  display: flex;
-  flex-direction: column;
-
-  .pricing-card-discount {
-    margin: 0;
-  }
-`,m=({price:o,taxRate:c,oldPrice:s,textSizes:r})=>j?n.jsxs(i,{children:[s&&n.jsx("div",{className:"pricing-card-discount",children:n.jsxs(d.Text,{size:(r==null?void 0:r.old)||"18",children:[l(s,c)," zł"]})}),n.jsxs(d.Text,{size:(r==null?void 0:r.new)||"16",children:[l(o,c)," zł"]})]}):n.jsxs(i,{children:[n.jsxs(d.Text,{size:(r==null?void 0:r.new)||"16",children:[l(o,c)," zł"]}),s&&n.jsx("div",{className:"pricing-card-discount",children:n.jsxs(d.Text,{size:(r==null?void 0:r.old)||"18",children:[l(s,c)," zł"]})})]});export{m as P};
