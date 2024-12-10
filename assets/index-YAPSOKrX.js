baf8dc46010306a8367269900313649a0ee08e0e
victazzz <przemyslaw.wiktorskii@gmail.com>
2024-12-10 09:59:17 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@51d36662c06fc583039a6ded12f651ca220db2d8 🚀

--- assets/index-YAPSOKrX.js
+++
@@ -1,8 +0,0 @@
-import{q as h,h as u,o as m,j as n,y as c,W as l}from"./index-fHnPfKbw.js";const o=h.div`
-  display: flex;
-  flex-direction: column;
-
-  .pricing-card-discount {
-    margin: 0;
-  }
-`,f=({price:a,taxRate:s,oldPrice:d,textSizes:r,isFree:i})=>{const{t:j}=u();return m?n.jsxs(o,{children:[d&&n.jsx("div",{className:"pricing-card-discount",children:n.jsxs(c.Text,{size:(r==null?void 0:r.old)||"18",children:[l(d,s)," zł"]})}),n.jsxs(c.Text,{size:(r==null?void 0:r.new)||"16",children:[l(a,s)," zł"]})]}):i?n.jsx(o,{children:n.jsx(c.Text,{size:(r==null?void 0:r.new)||"16",children:j("CoursesPage.Free")})}):n.jsxs(o,{children:[n.jsxs(c.Text,{size:(r==null?void 0:r.new)||"16",children:[l(a,s)," zł"]}),d&&n.jsx("div",{className:"pricing-card-discount",children:n.jsxs(c.Text,{size:(r==null?void 0:r.old)||"18",children:[l(d,s)," zł"]})})]})};export{f as P};
---
+++ assets/my-certificates-CgNQ0KUp.js
@@ -0,0 +1,29 @@
+import{q as f,r as x,N as C,h as u,j as t,y as a,V as p,n as j,T as h,a0 as y,bU as b}from"./index-HzhV-0zX.js";import{P}from"./index-DWc8K4At.js";import{u as T}from"./useDownloadCertificate-1sW7Pfdb.js";import"./index-BWoS6tNy.js";const w=f.section`
+  .empty-certificates-message {
+    background: ${({theme:e})=>e.mode==="dark"?e.gray1:e.gray5};
+  }
+  .buttons-container {
+    margin-top: 20px;
+    display: flex;
+
+    align-items: center;
+    justify-content: flex-start;
+
+    .download-btn {
+      all: unset;
+      display: flex;
+      align-items: center;
+      gap: 6px;
+      cursor: pointer;
+      color: ${({theme:e})=>e.primaryColor};
+      p {
+        color: ${({theme:e})=>e.primaryColor};
+      }
+    }
+  }
+  .certificate-card {
+    margin-bottom: 24px;
+  }
+`,E=()=>{var o,l,n,c,d;const{certificates:e,fetchCertificates:r}=x.useContext(C.EscolaLMSContext),{t:s}=u(),{downloadCertificate:g,loadingId:m}=T();return x.useEffect(()=>{r()},[r]),t.jsx(t.Fragment,{children:t.jsxs(w,{children:[((o=e.list)==null?void 0:o.data.length)===0&&t.jsx(a.Text,{className:"empty-certificates-message",children:t.jsx("strong",{children:s("MyProfilePage.EmptyCertificates")})}),t.jsx(p.Row,{children:e&&((l=e==null?void 0:e.list)==null?void 0:l.data)&&((n=e.list)==null?void 0:n.data.length)>0&&((d=(c=e==null?void 0:e.list)==null?void 0:c.data)==null?void 0:d.filter(i=>i.title).map(i=>t.jsx(p.Col,{lg:4,children:t.jsx(j.CertificateCard,{uptitle:t.jsx(a.Text,{size:"13",children:s("CoursePage.CourseTitle")}),title:t.jsx(h.Title,{level:4,as:"h3",children:i.title}),dateUptitle:t.jsx(a.Text,{size:"13",children:s("CoursePage.CertificateDate")}),date:t.jsx(a.Text,{noMargin:!0,size:"16",bold:!0,children:new Date(i.created_at).toLocaleDateString("pl-PL")}),actions:t.jsx("div",{className:"buttons-container",children:m===i.id?t.jsx(y,{width:"15px",height:"15px"}):t.jsxs("button",{className:"download-btn",onClick:()=>g(i.id,i.title),children:[t.jsx(b,{})," ",t.jsx(a.Text,{bold:!0,size:"13",children:s("CoursePage.DownloadCertificate")})]})})})})))})]})})},L=f.div`
+  margin-top: 20px;
+`,k=()=>{const{t:e}=u();return t.jsx(P,{title:e("MyProfilePage.MyCertificates"),children:t.jsx(L,{children:t.jsx(E,{})})})};export{k as default};
