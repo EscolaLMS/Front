362cb90f42fa4a13b463bfa85f1dc092789e2886
victazzz <przemyslaw.wiktorskii@gmail.com>
2024-12-10 11:03:27 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@d8fe38b6f8f65ef4aeb85d2158e9e187c169528c 🚀

---
+++ assets/index-C-NBZkvq.js
@@ -0,0 +1,13 @@
+import{q as n,h as o,k as a,j as t,v as i,C as r,T as c,y as l,z as u,m as x}from"./index-Bmxyj2q8.js";const d=n.div`
+  height: calc(100vh - 452px);
+  padding-top: 122px;
+  background-color: ${({theme:e})=>e.gray4};
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    row-gap: 30px;
+  }
+`,m=()=>{const{t:e}=o(),s=a();return t.jsx(i,{children:t.jsx(d,{children:t.jsx(r,{children:t.jsxs("div",{className:"content",children:[t.jsx(c.Title,{level:3,children:e("Custom404Page.Info")}),t.jsx(l.Text,{children:e("Custom404Page.NotFound")}),t.jsx(u.Button,{onClick:()=>s.push(x.home),children:e("Home")})]})})})})};export{m as default};
