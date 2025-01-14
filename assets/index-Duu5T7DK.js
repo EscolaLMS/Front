0dcfdf85b57d753883a126c1a003b5cf9c676cf4
victazzz <przemyslaw.wiktorskii@gmail.com>
2025-01-14 09:20:04 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@03ed335e492ebe7a6b4cb348c67f7792691b8599 🚀

---
+++ assets/index-Duu5T7DK.js
@@ -0,0 +1,11 @@
+import{q as o,j as a,bT as s}from"./index-BJhtOcuL.js";const t=o.div`
+  margin-bottom: 36px;
+  p {
+    color: ${({theme:r})=>r.mode==="dark"?r.gray5:r.gray2}!important;
+  }
+  a {
+    text-decoration: none;
+    color: ${({theme:r})=>r.mode==="dark"?r.gray5:r.gray2}!important;
+  }
+`,n=({items:r})=>a.jsx(t,{children:a.jsx(s.BreadCrumbs,{items:r})});export{n as B};
+//# sourceMappingURL=index-Duu5T7DK.js.map
