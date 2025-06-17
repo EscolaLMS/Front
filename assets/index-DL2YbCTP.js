cadbed634205711f14656f3a4b9118a6f287058c
victazzz <przemyslaw.wiktorskii@gmail.com>
2025-06-17 13:28:38 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@cf8e7ced1f775f0618e62d907c5016fef8e791cc 🚀

---
+++ assets/index-DL2YbCTP.js
@@ -0,0 +1,11 @@
+import{j as a,bP as o,q as s}from"./index-D5UVe-mj.js";const t=s.div`
+  margin-bottom: 36px;
+  p {
+    color: ${({theme:r})=>r.mode==="dark"?r.gray5:r.gray2}!important;
+  }
+  a {
+    text-decoration: none;
+    color: ${({theme:r})=>r.mode==="dark"?r.gray5:r.gray2}!important;
+  }
+`,n=({items:r})=>a.jsx(t,{children:a.jsx(o.BreadCrumbs,{items:r})});export{n as B};
+//# sourceMappingURL=index-DL2YbCTP.js.map
