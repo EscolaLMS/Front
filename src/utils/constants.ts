17424e06301c54f1c43f7d5847970f8d280abcd6
mateusz.bieniek <mateusz.bieniek@escolasoft.com>
2026-04-17 09:22:11 +0200

    Add enable/disable ai analysis by :prop from backend and participant count

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const JITSY_TUTOR_INTERVAL = 15000;
 export const JITSY_ANALYTICS_INTERVAL = 1000;
 // Number of people needed to start attention and emotion ai analyze
-export const JITSY_AI_MIN_PARTICIPANT_COUNT = 2;
+export const JITSY_AI_MIN_PARTICIPANT_COUNT = 3;
