60de0106122b9c55976b3c39fecfd9bc85be1814
mateusz.bieniek <mateusz.bieniek@escolasoft.com>
2026-04-30 14:25:56 +0200

    Hide hovered panel when ai analytics is off

--- src/components/MeetingAnalyticsOverlay/MeetingAnalyticsOverlay.tsx
+++ src/components/MeetingAnalyticsOverlay/MeetingAnalyticsOverlay.tsx
@@ -242,6 +242,7 @@ export default function MeetingAnalyticsOverlay({
       setAttentionData([]);
       setEmotionHistory([]);
       setShouldBreak(false);
+      setHoveredPanel(null);
     }
   }, [shouldRunAnalytics]);
 
