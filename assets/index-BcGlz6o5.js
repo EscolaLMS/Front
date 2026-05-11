d49da3d8a321faaf3f22169c21758df720168e5e
mateuszBieniekEscola <98316108+mateuszBieniekEscola@users.noreply.github.com>
2026-05-11 11:00:41 +0000

    Deploying to gh-pages from @ EscolaLMS/Front@0d044f28b067e335c14d0ba6dd4054c2545c9c54 🚀

---
+++ assets/index-BcGlz6o5.js
@@ -0,0 +1,12 @@
+import{j as o,q as s}from"./index-DG-N9xU8.js";const e="/assets/no-picture-sHwVEW-e.png",t=s.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid
+    ${({theme:r})=>r.mode==="light"?r.gray4:r.gray2};
+  img {
+    &:hover {
+      transform: none !important;
+    }
+  }
+`,i=()=>o.jsx(t,{children:o.jsx("img",{src:e,alt:""})});export{i as C};
+//# sourceMappingURL=index-BcGlz6o5.js.map
