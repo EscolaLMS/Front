dbe666f24e318673d79d46b8e829009f60b3fac3
mateusz.bieniek <mateusz.bieniek@escolasoft.com>
2026-07-24 09:54:24 +0200

    Add eu banner in footer

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,5 @@ export const JITSY_TUTOR_INTERVAL = 15000;
 export const JITSY_ANALYTICS_INTERVAL = 1000;
 // Number of people needed to start attention and emotion ai analyze
 export const JITSY_AI_MIN_PARTICIPANT_COUNT = 3;
+
+export const EU_BANNER_LINK = "https://rekomender2.wellms.io/";
